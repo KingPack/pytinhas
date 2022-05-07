efc3739a6d1c45313128e9d55227fbcda77a12f0
Hendrek Rodrigues <llhendrek@gmail.com>
2022-05-06 22:11:16 -0300

    Testing github actions

--- scripts/Python/my_pytinhas.py
+++ scripts/Python/my_pytinhas.py
@@ -7,22 +7,18 @@ def atualiza_dados():
     while 1:
         resposta = requests.get('http://192.168.100.21/')
         if resposta.status_code == 200:
-            
             dados = resposta.json()
             horas = datetime.now().strftime('%H:%M:%S')
             print(f'\nHorário: {horas}')
             print(f'Temperatura: {dados["temperatura"]} °C')
             print(f'Umidade: {dados["umidade"]} %')
             print(f'Agua: {dados["agua"]} ')
-
-
             with open('dados.txt', 'a') as arquivo:
-                arquivo.write(f'{horas} - {dados["temperatura"]} - {dados["umidade"]} - {dados["agua"]}\n')
-
+                arquivo.write('test')
             time.sleep(60)
-        
         else:
             print('Erro ao atualizar dados')
             time.sleep(60)
+
 
 atualiza_dados()
