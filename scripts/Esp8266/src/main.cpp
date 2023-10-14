c0d28c9b042d1e62bef949d04403e9599e61e145
Hendrek Rodrigues <llhendrek@gmail.com>
2023-10-14 20:24:24 -0300

    feat: ESP-8266 - Refactore project and create new features

--- scripts/Esp8266/src/main.cpp
+++ scripts/Esp8266/src/main.cpp
@@ -1,58 +1,153 @@
+#include <secrets.h>
 #include <DHT.h>
 #include <DHT_U.h>
-#include "DHTesp.h"
+#include <DHTesp.h>
+#include <Adafruit_Sensor.h>
 #include <Arduino.h>
-#include <WiFiClient.h>
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
-#include <Adafruit_Sensor.h>
-#include <secrets.h>
+#include <WiFiClient.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
-#include <time.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include <Ticker.h>
 
-#define THINGNAME "pytinhas"
+
 #define WATER A0
-#define AWS_IOT_PUBLISH_TOPIC   "pytinhas/pub"
-#define AWS_IOT_SUBSCRIBE_TOPIC "pytinhas/sub"
+#define SUN_RELAY 13    // D7
+#define WIND_RELAY 15 // D8
 
+
+Ticker timer;
+Ticker timedRelayControl;
+tm timeinfo;
 
 WiFiClientSecure net;
-DHT_Unified dht(2, DHT11);
+WiFiUDP ntpUDP;
 ESP8266WebServer server(80);
+PubSubClient client(net);
+
+NTPClient timeClient(ntpUDP, NTP_SERVER, TIME_ZONE * 3600, DAYLIGHT_OFFSET_SEC);
+
+DHT_Unified dht(14, DHT11); // D5
+
 
 BearSSL::X509List cert(cacert);
 BearSSL::X509List client_crt(client_cert);
 BearSSL::PrivateKey key(privkey);
 
-PubSubClient client(net);
-
-time_t now;
-time_t nowish = 1510592825;
 
 unsigned long lastMillis = 0;
-unsigned long previousMillis = 0;
 const long interval = 5000;
+bool relayState = LOW;
+
+
+void controlRelay(bool ligar, int relayPin) {
+  digitalWrite(relayPin, ligar ? LOW : HIGH);
+  relayState = ligar;
+}
+
+
+void toggleRelay(int relayPin) {
+  controlRelay(!digitalRead(relayPin), relayPin);
+}
+
+
+void handleWindOn() {
+  controlRelay(true, WIND_RELAY);
+  server.send(200, "text/plain", "Wind Relay Status: " + WIND_RELAY);
+}
+
+
+void handleWindOff() {
+  controlRelay(false, WIND_RELAY);
+  server.send(200, "text/plain", "Wind Relay Status: " + WIND_RELAY);
+}
+
+
+void handleSunOn() {
+  controlRelay(true, SUN_RELAY);
+  server.send(200, "text/plain", "Sun Relay Status: " + SUN_RELAY);
+}
+
+
+void handleSunOff() {
+  controlRelay(false, SUN_RELAY);
+  server.send(200, "text/plain", "Sun Relay Status: " + SUN_RELAY);
+}
+
+
+void updateTime() {
+  timeClient.update();
+  time_t now = timeClient.getEpochTime();
+  gmtime_r(&now, &timeinfo);
+}
+
+
+void setupTimedRelayControl() {
+  String timeOn = "06:30";
+  String timeOff = "18:30";
+
+  pinMode(SUN_RELAY, OUTPUT);
+
+  timedRelayControl.attach(420, [timeOn, timeOff]() {
+    timeClient.update();
+    String currentTime = timeClient.getFormattedTime();
+
+    if (strcmp(currentTime.c_str(), timeOn.c_str()) == 0) {
+      controlRelay(true, SUN_RELAY);
+
+    } else if (strcmp(currentTime.c_str(), timeOff.c_str()) == 0) {
+      controlRelay(false, SUN_RELAY); 
+    }
+  });
+}
 
 
 void handleRoot() {
+
+  bool windRelayState = digitalRead(WIND_RELAY) == HIGH;
+  bool sunRelayState = digitalRead(SUN_RELAY) == HIGH;
+
+  String response = "Device Status:\n";
+  response += "Wind Relay: " + String(windRelayState ? "ON" : "OFF") + "\n";
+  response += "Sun Relay: " + String(sunRelayState ? "ON" : "OFF") + "\n";
+
+  server.send(200, "text/plain", response);
+}
+
+
+void getDHT11() {
   char jsonBuffer[512];
+  char dateStr[20];
+  char timeStr[20];
+
   StaticJsonDocument<200> json;
-  
+
   sensors_event_t temperature_float;
   sensors_event_t humidity_float;
-  
+
+  timeClient.update();
+
+  updateTime();
+
+  strftime(dateStr, sizeof(dateStr), "%Y-%m-%d", &timeinfo);
+  strftime(timeStr, sizeof(timeStr), "%H:%M:%S", &timeinfo);
+
   dht.temperature().getEvent(&temperature_float);
   dht.humidity().getEvent(&humidity_float);
 
   json["humidity"] = humidity_float.relative_humidity;
   json["temperature"] = temperature_float.temperature;
-  json["time"] = millis();
+  json["date"] = dateStr;
+  json["time"] = timeStr;
+
   serializeJson(json, jsonBuffer);
 
-  server.send(200, "application/json", jsonBuffer);  // 70.0e23.0
+  server.send(200, "application/json", jsonBuffer);
 }
 
 
@@ -62,114 +157,133 @@ void handleNotFound() {
 }
 
 
-void getDHT11() {
-  StaticJsonDocument<200> json;
-  char jsonBuffer[512];
-  
-  sensors_event_t temperature_float;
-  sensors_event_t humidity_float;
-  
-  dht.temperature().getEvent(&temperature_float);
-  dht.humidity().getEvent(&humidity_float);
-  
-  json["time"] = millis();
-  json["humidity"] = humidity_float.relative_humidity;
-  json["temperature"] = temperature_float.temperature;
+void NTPConnect(void) {
+  Serial.println("Setting time using SNTP");
 
-  serializeJson(json, jsonBuffer);
-
-  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
-}
-
- 
-void NTPConnect(void)
-{
-  Serial.print("Setting time using SNTP");
-  configTime(TIME_ZONE * 3600, 0 * 3600, "pool.ntp.org", "time.nist.gov");
-  now = time(nullptr);
-  while (now < nowish)
-  {
-    delay(500);
+  while (!timeClient.update()) {
+    delay(1000);
     Serial.print(".");
-    now = time(nullptr);
   }
-  Serial.println("done!");
-  struct tm timeinfo;
+
+  time_t now = timeClient.getEpochTime();
   gmtime_r(&now, &timeinfo);
+
+  Serial.println("done!");
   Serial.print("Current time: ");
-  Serial.print(asctime(&timeinfo));
+  Serial.println(asctime(&timeinfo));
 }
- 
 
 
-void messageReceived(char *topic, byte *payload, unsigned int length)
-{
-  Serial.print("Received [");
+void messageReceived(char* topic, byte* payload, unsigned int length) {
+  Serial.println("Received [");
   Serial.print(topic);
   Serial.print("]: ");
-  for (int i = 0; i < length; i++)
-  {
+  for (unsigned int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
 }
 
 
-void connectAWS()
-{
+void handleRelayControl(bool windRelay, bool sunRelay) {
+  digitalWrite(WIND_RELAY, windRelay ? HIGH : LOW);
+  digitalWrite(SUN_RELAY, sunRelay ? HIGH : LOW);
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Topic: ");
+  Serial.println(topic);
+  Serial.print("Payload: ");
+
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+
+  Serial.println();
+
+  StaticJsonDocument<200> doc;
+
+  DeserializationError error = deserializeJson(doc, payload, length);
+
+  if (error == DeserializationError::Ok) {
+    Serial.println("Valid JSON received:");
+
+    JsonObject obj = doc.as<JsonObject>();
+
+    if (obj.containsKey("message") && obj.containsKey("wind_relay") && obj.containsKey("sun_relay")) {
+      const char* message = obj["message"];
+      bool windRelay = obj["wind_relay"];
+      bool sunRelay = obj["sun_relay"];
+
+      Serial.print("Message: ");
+      Serial.println(message);
+      Serial.print("Wind Relay: ");
+      Serial.println(windRelay);
+      Serial.print("Sun Relay: ");
+      Serial.println(sunRelay);
+
+      handleRelayControl(windRelay, sunRelay);
+    } else {
+      Serial.println("Invalid JSON format. Missing required keys.");
+    }
+  } else {
+    Serial.println("Error deserializing JSON. Invalid payload.");
+  }
+}
+
+
+void connectAWS() {
   delay(3000);
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
- 
+
   Serial.println(String("Attempting to connect to SSID: ") + String(WIFI_SSID));
- 
-  while (WiFi.status() != WL_CONNECTED)
-  {
+
+  while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(1000);
   }
- 
+
+  Serial.println("");
   NTPConnect();
- 
+
   net.setTrustAnchors(&cert);
   net.setClientRSACert(&client_crt, &key);
- 
+
   client.setServer(MQTT_HOST, 8883);
   client.setCallback(messageReceived);
- 
- 
+  client.setCallback(callback);
+
   Serial.println("Connecting to AWS IOT");
- 
-  while (!client.connect(THINGNAME))
-  {
+
+  while (!client.connect(THINGNAME)) {
     Serial.print(".");
     delay(1000);
   }
- 
+
   if (!client.connected()) {
     Serial.println("AWS IoT Timeout!");
     return;
   }
-  // Subscribe to a topic
-  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
- 
+
+  client.subscribe(pytinhaSub);
+  client.subscribe(mqttTopic);
+
   Serial.println("AWS IoT Connected!");
 }
 
 
-void connectWIFI(){
-
+void connectWIFI() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  
-  // Wait for connection
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-
   }
 
-  Serial.print("Connected to ");
+  Serial.println("Connected to ");
   Serial.println(WIFI_SSID);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -177,72 +291,68 @@ void connectWIFI(){
   if (MDNS.begin("esp8266")) {
     Serial.println("MDNS responder started");
   }
-  
-  // Urls 
+
+  // Routes
   server.on("/", handleRoot);
   server.on("/dht11", getDHT11);
+  server.on("/wind-on", HTTP_GET, handleWindOn);
+  server.on("/wind-off", HTTP_GET, handleWindOff);
+  server.on("/sun-on", HTTP_GET, handleSunOn);
+  server.on("/sun-off", HTTP_GET, handleSunOff);
   server.onNotFound(handleNotFound);
   server.begin();
   Serial.println("HTTP server started");
 }
 
-void publishMessage()
-{
+
+void publishMessage() {
   StaticJsonDocument<200> json;
+  char jsonBuffer[512];
+  char dateStr[20];
+  char timeStr[20];
+
+  timeClient.update();
+  updateTime();
+  strftime(dateStr, sizeof(dateStr), "%Y-%m-%d", &timeinfo);
+  strftime(timeStr, sizeof(timeStr), "%H:%M:%S", &timeinfo);
+
   sensors_event_t temperature_float;
   sensors_event_t humidity_float;
-  
+
   dht.temperature().getEvent(&temperature_float);
   dht.humidity().getEvent(&humidity_float);
-  
-  json["time"] = millis();
+
   json["humidity"] = humidity_float.relative_humidity;
   json["temperature"] = temperature_float.temperature;
-  char jsonBuffer[512];
-  serializeJson(json, jsonBuffer); // print to client
- 
+  json["date"] = dateStr;
+  json["time"] = timeStr;
+
+  serializeJson(json, jsonBuffer);
+
   client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
 }
 
-void setup(void){
+
+void setup(void) {
   Serial.begin(115200);
-  // connectWIFI();
+  connectWIFI();
+  configTime(TIME_ZONE * 3600, 0 * 3600, NTP_SERVER, "time.nist.gov");
   connectAWS();
   dht.begin();
+  setupTimedRelayControl();
 }
 
 
-void loop(void){
+void loop(void) {
+  updateTime();
   server.handleClient();
-  sensors_event_t temperature_float;
-  sensors_event_t humidity_float;
-  dht.temperature().getEvent(&temperature_float);
-  dht.humidity().getEvent(&humidity_float);
-  
-  String texto_display = " " + String(humidity_float.relative_humidity) + " | " + String(temperature_float.temperature) + " ";
-  
-  Serial.print("Humidity (%): ");
-  Serial.print(humidity_float.relative_humidity, 2);
-  Serial.print("\t");
-  Serial.print("Temperature (C): ");
-  Serial.print(temperature_float.temperature, 2);
-  Serial.print("\t");
-  // Serial.print("Water: ");
-  // Serial.print(analogRead(WATER));
-  // Serial.print("\t");
-  Serial.println();
   delay(2000);
-  now = time(nullptr);
- 
-  if (!client.connected())
-  {
+
+  if (!client.connected()) {
     connectAWS();
-  }
-  else
-  {
+  } else {
     client.loop();
-    if (millis() - lastMillis > 5000)
-    {
+    if (millis() - lastMillis > 5000) {
       lastMillis = millis();
       publishMessage();
     }
--- scripts/Esp8266/src/secrets.h
+++ scripts/Esp8266/src/secrets.h
@@ -1,18 +1,47 @@
 #include <pgmspace.h>
 
+
 #define SECRET
-#define THINGNAME "pytinhas"
 
-const char WIFI_SSID[] = "Falconfibra Francielly_2.4G";
-const char WIFI_PASSWORD[] = "Pelucia12";
+#define AWS_IOT_PUBLISH_TOPIC "pytinha/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC "pytinha/sub"
+#define THINGNAME "pytinha"
 
+
+const char* pytinhaSub = "pytinha/sub";
+const char* mqttTopic = "relay-control";
+
+const char WIFI_SSID[] = "xxxxxxxx";
+const char WIFI_PASSWORD[] = "xxxxxxxx";
+
+const char MQTT_HOST[] = "-ats.iot.us-east-2.amazonaws.com";
+
+const char *NTP_SERVER = "pool.ntp.org";
+const long GMT_OFFSET_SEC = 0;
+const int DAYLIGHT_OFFSET_SEC = 3600;
 int8_t TIME_ZONE = -3;
 
-const char MQTT_HOST[] = "a22fqccedfv11r-ats.iot.us-east-1.amazonaws.com";
 
 static const char cacert[] PROGMEM = R"EOF(
 -----BEGIN CERTIFICATE-----
-
+MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
+ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6
+b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL
+MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv
+b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj
+ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM
+9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw
+IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6
+VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L
+93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm
+jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC
+AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA
+A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI
+U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs
+N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv
+o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU
+5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy
+rqXRfboQnoZsG4q5WTP468SQvvG5
 -----END CERTIFICATE-----
 )EOF";
 
@@ -20,6 +49,7 @@ static const char cacert[] PROGMEM = R"EOF(
 static const char client_cert[] PROGMEM =R"KEY(
 -----BEGIN CERTIFICATE-----
 
+-----END CERTIFICATE-----
 )KEY";
 
 
